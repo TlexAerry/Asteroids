387a67fe06139ec2318ecb300c638dd9c9e70259
TlexAerry <alexterry987@proton.me>
2025-02-15 22:17:36 +0000

    10) started using containers to manage objects

--- main.py
+++ main.py
@@ -14,14 +14,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.containers = (updateable, drawable)
     player1 = player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill((0,0,0))
-        player1.update(dt)
-        player1.draw(screen)
+        updateable.update(dt)
+        
+        for object in drawable:
+            object.draw(screen)
+        
         pygame.display.flip()  
         dt = clock.tick(60) / 1000
 
