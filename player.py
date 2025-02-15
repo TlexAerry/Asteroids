587fb76920f158b7b2466f63666af752d59096bf
TlexAerry <alexterry987@proton.me>
2025-02-15 21:32:31 +0000

    8) WE GOT ROTATIONgit log --oneline!

--- player.py
+++ player.py
@@ -16,3 +16,15 @@ class player(CircleShape):
 
     def draw(self,screen):
         pygame.draw.polygon(screen,(255,255,255),self.triangle(),2)
+
+    def rotate(self, dt):
+        self.rotation += dt * PLAYER_TURN_SPEED 
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
